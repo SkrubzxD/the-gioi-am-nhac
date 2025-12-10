7f1f917abd057714466e20c72de5529903a081dc
reboot-protocol <189536851+reboot-protocol@users.noreply.github.com>
2025-12-10 17:22:00 +0700

    add quiz

--- manual_add_tables.py
+++ manual_add_tables.py
@@ -2,23 +2,15 @@ import sqlite3
 conn = sqlite3.connect('example.db')  # Creates a new database file if it doesn’t exist
 cursor = conn.cursor()
 table_creation_query = """
-    CREATE TABLE Thesis (
+    CREATE TABLE Question (
         ID VARCHAR(255) NOT NULL PRIMARY KEY,
-        Author CHAR(25) NOT NULL,
-        Category CHAR(25),
-        Score INT
+        QName VARCHAR(255) NOT NULL,
+        CorrectA CHAR(25) NOT NULL,
+        DecoyB CHAR(25) NOT NULL,
+        DecoyC CHAR(25) NOT NULL,
+        DecoyD CHAR(25) NOT NULL
     );
 """
 cursor.execute(table_creation_query)
 cursor.close()
 
-"""
-class Thesis(db.Model):
-    ID = db.Column(db.String(25), primary_key = True, nullable=False)
-    Author = db.Column(db.String(25),nullable = False)
-    Category = db.Column(db.String(25), nullable = False)
-    Supervisor = db.Column(db.String(25), nullable = False)
-    Name = db.Column(db.String(255),nullable = False)
-    File_name = db.Column(db.String(25), nullable = False)
-
-"""
